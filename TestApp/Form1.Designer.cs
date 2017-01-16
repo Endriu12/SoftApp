2d079f05360d9854c4806d33411caad47cd2fa32
hiichaki <hiichaki350@gmail.com>
2017-01-16 15:49:42 +0200

    My Application is Done

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TestApp
+{
+    partial class TortoiseYukon
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TortoiseYukon));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cancel = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showProgramToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // showProgramToolStripMenuItem
+            // 
+            this.showProgramToolStripMenuItem.Name = "showProgramToolStripMenuItem";
+            this.showProgramToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showProgramToolStripMenuItem.Text = "Show Program";
+            this.showProgramToolStripMenuItem.Click += new System.EventHandler(this.showProgramToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "YukonSoftware";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // OK
+            // 
+            this.OK.Cursor = System.Windows.Forms.Cursors.Default;
+            this.OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OK.Location = new System.Drawing.Point(69, 82);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(67, 23);
+            this.OK.TabIndex = 2;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter your password here";
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(182, 82);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 4;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TortoiseYukon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 112);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TortoiseYukon";
+            this.Text = "YukonSoftware";
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showProgramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancel;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+namespace TestApp
+{
+    public partial class TortoiseYukon : Form
+    {
+        //add method who use WinAPI
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindowEx(IntPtr parentHandle, int childAfter, string className, string windowTitle);
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        const int WM_CHAR = 0x0102;
+        string path = @"password.txt";
+        string program = "TortoiseGit -  git CLI stdin wrapper";
+        string childwindow = "Edit";
+
+
+        public TortoiseYukon()
+        {
+            InitializeComponent();
+        }
+
+        private void showProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter write_text;  //Class file recording
+            FileInfo file = new FileInfo(path);
+            write_text = file.AppendText(); //if the file does not exist it will be created
+            write_text.WriteLine(textBox1.Text); 
+            write_text.Close(); 
+
+            Timer tmrShow = new Timer();
+            tmrShow.Interval = 5000; // 5 
+            tmrShow.Tick += tmrShow_Tick; // event
+            tmrShow.Enabled = true;//timer start
+
+            this.Hide();
+        }
+
+        private void tmrShow_Tick(object sender, EventArgs e)
+        {
+            StreamReader streamReader = new StreamReader(path); //Open the file for reading
+            string str = ""; 
+            while (!streamReader.EndOfStream) 
+            {
+                str += streamReader.ReadLine(); //The variable str row to record the contents of the file
+            }
+            IntPtr hWnd = FindWindow(null, program);
+            if (hWnd.Equals(IntPtr.Zero))
+                {
+                   
+                }
+                else  {
+                    IntPtr hWndEdit = FindWindowEx(hWnd, 0, childwindow, null);
+                    SetForegroundWindow(hWnd);
+                    SendKeys.SendWait(str);
+                    SendKeys.SendWait("~");//enter 
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        } 
+    }
+}
